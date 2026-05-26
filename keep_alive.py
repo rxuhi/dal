4a5fe693a131af8a8059c7afc89fe59a24426c6c
rxuhi <hyorim4640@gmail.com>
2026-05-26 18:12:19 +0900

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "봇이 온라인 상태입니다."
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
